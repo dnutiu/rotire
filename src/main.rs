f53e17ad127138db2a4bc91f718358060625a51a
Denis Nutiu <dnutiu@nuculabs.de>
2025-03-26 23:25:13 +0200

    implement action for archive and delete

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod rotire;
 use clap::Parser;
 use env_logger;
 use log::{error, info};
+use crate::rotire::RotireAction;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -15,13 +16,28 @@ struct Args {
     /// How many items to keep, defaults to 4.
     #[arg(short('k'), long, default_value_t = 4)]
     keep_n: i32,
+
+    #[arg(
+        default_value = "archive-delete",
+        value_parser = ["archive-delete", "delete"],
+        help = "Select the action rotire should run."
+    )]
+    action: String
 }
 
 fn main() {
     env_logger::init();
     let args = Args::parse();
     let rotire = rotire::Rotire::new(args.directory);
-    let result = rotire.run(args.keep_n);
+    let mut action: RotireAction = RotireAction::ArchiveAndDelete;
+    match args.action.as_str() {
+        "delete" => {
+            action = RotireAction::Delete
+        }
+        _ => {}
+    }
+
+    let result = rotire.run(args.keep_n, action);
     if let Ok(result) = result {
         info!("Rotire ran successfully: {result}.")
     } else {
--- src/rotire.rs
+++ src/rotire.rs
@@ -1,14 +1,21 @@
 use anyhow::{anyhow, Result};
 use std::fmt::{Display, Formatter};
 use std::fs;
-use std::fs::Metadata;
+use std::fs::{File, Metadata};
 use std::os::linux::fs::MetadataExt;
 use std::path::{Path, PathBuf};
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::time::{SystemTime, UNIX_EPOCH};
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use log::error;
 
-pub enum RotireMode {
+/// RotireAction is the action that rotire will perform on run.
+pub enum RotireAction {
     /// Delete mode deletes files.
     Delete,
+    /// Archive files and delete older ones.
+    ArchiveAndDelete
 }
 
 #[derive(Debug)]
@@ -89,7 +96,63 @@ impl Rotire {
         Ok(files)
     }
 
-    pub fn run(&self, keep_max_files: i32) -> Result<RotireResult> {
+    fn execute_action(&self, files: Vec<&RotireFile>, action: RotireAction) -> Result<RotireResult> {
+        let mut result = RotireResult::new();
+        match action {
+            RotireAction::Delete => {
+                for file in files {
+                    result.inc_affected_files_size(file.metadata.st_size());
+                    result.inc_affected_files();
+
+                    // do action
+                    if let Err(result) = fs::remove_file(file.path.clone()) {
+                        error!("failed to delete file {0:?}: {1}", file.path, result)
+                    }
+                }
+            }
+            RotireAction::ArchiveAndDelete => {
+                let timestamp = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .expect("Time went backwards")
+                    .as_millis();
+
+                let mut archive_path = PathBuf::from(self.directory.clone());
+                archive_path = archive_path.join(format!("rotire-archive-{0}.tar.gz", timestamp));
+                let tar_gz = File::create(archive_path)?;
+                let enc = GzEncoder::new(tar_gz, Compression::default());
+                let mut tar = tar::Builder::new(enc);
+                files.iter().for_each(|file| {
+                    result.inc_affected_files_size(file.metadata.st_size());
+                    result.inc_affected_files();
+
+                    // do action
+                    let file_path: &PathBuf = &file.path.clone();
+                    let file_result = File::open(file_path);
+                    match file_result {
+                        Ok(mut file_handle) => {
+                            if let Err(result) = tar.append_file(file_path.file_name().unwrap(), &mut file_handle) {
+                                error!("failed to archive file {0:?}: {1}", file_path, result)
+                            } else {
+                                if let Err(result) = fs::remove_file(file_path) {
+                                    error!("failed to delete file {0:?}: {1}", file_path, result)
+                                }
+                            }
+                        }
+                        Err(err) => {
+                            error!("failed to open file {0:?}: {1}", file_path, err)
+                        }
+                    }
+
+
+                });
+                tar.finish()?;
+
+            }
+        }
+        Ok(result)
+    }
+
+    pub fn run(&self, keep_max_files: i32, action: RotireAction) -> Result<RotireResult> {
         if self.is_running.load(Ordering::Relaxed) {
             return Err(anyhow!(
                 "Can't start another run while one is already running."
@@ -110,14 +173,7 @@ impl Rotire {
         );
 
         // Execute action and record result
-        let mut result = RotireResult::new();
-        for file in files.iter().rev().skip(keep_max_files as usize) {
-            result.inc_affected_files_size(file.metadata.st_size());
-            result.inc_affected_files();
-
-            // do action
-            fs::remove_file(file.path.clone())?;
-        }
+        let result = self.execute_action(files.iter().rev().skip(keep_max_files as usize).collect(), action)?;
 
         self.is_running.store(false, Ordering::Relaxed);
         Ok(result)
